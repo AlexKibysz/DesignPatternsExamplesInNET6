4fae55f435508c6e833ed7a217c003e8b7c779c2
Alex <Alexander.Kibysz@outlook.com>
2024-09-25 10:57:37 -0300

    Change PersonaMem to apply SRP

---
+++ PatronesComportamiento/5.Memento/Helper/RandomStringGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronesComportamiento._5.Memento.Helper
+{
+    public static class RandomStringGenerator
+    {
+        public static string GenerateRandomString(int length = 10)
+        {
+            const string allowedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var result = string.Empty;
+            var random = new Random();
+
+            while (length > 0)
+            {
+                result += allowedSymbols[random.Next(0, allowedSymbols.Length)];
+                Thread.Sleep(12);
+                length--;
+            }
+
+            return result;
+        }
+    }
+}
--- PatronesComportamiento/5.Memento/Originator/PersonaMem.cs
+++ PatronesComportamiento/5.Memento/Originator/PersonaMem.cs
@@ -2,53 +2,30 @@
 {
     public class PersonaMem
     {
-        public int edad { get; set; }
-
-        public string nombre { get; set; }
-
-        public string apellido { get; set; }
-
-        public string profesion { get; set; }
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Profesion { get; set; }
 
         public PersonaMem(int edad, string nombre, string apellido, string profesion)
         {
-            this.edad = edad;
-            this.nombre = nombre;
-            this.apellido = apellido;
-            this.profesion = profesion;
-
-            Console.WriteLine($"Mi Estado Inicial es edad: {edad},nombre: {nombre}, apellido: {apellido}, profesion: {profesion} ");
+            Edad = edad;
+            Nombre = nombre;
+            Apellido = apellido;
+            Profesion = profesion;
+            Console.WriteLine($"Estado Inicial: Edad: {edad}, Nombre: {nombre}, Apellido: {apellido}, Profesion: {profesion}");
         }
 
-        public void cambioDeDatos()
+        public void CambioDeDatos()
         {
             Console.WriteLine("Persona: Estoy haciendo algo");
-            this.nombre = this.GenerateRandomString(30);
-            Console.WriteLine($"Persona: mi estado cambio a: edad: {edad},nombre: {nombre}, apellido: {apellido}, profesion: {profesion}");
+            Nombre = RandomStringGenerator.GenerateRandomString(30);
+            Console.WriteLine($"Estado Cambiado: Edad: {Edad}, Nombre: {Nombre}, Apellido: {Apellido}, Profesion: {Profesion}");
         }
 
-        //ojo que esto viola Principio resp unica!!! solo a modo de ejemplo
-        private string GenerateRandomString(int length = 10)
+        public MementoPersona GuardarHistorial(DateTime datetime)
         {
-            string allowedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string result = string.Empty;
-
-            while (length > 0)
-            {
-                result += allowedSymbols[new Random().Next(0, allowedSymbols.Length)];
-
-                Thread.Sleep(12);
-
-                length--;
-            }
-
-            return result;
-        }
-
-        public MementoPersona guardarHistorial(DateTime datetime)
-        {
-            PersonaMem histPerson = new PersonaMem(this.edad, this.nombre, this.apellido, this.profesion);
-            return new MementoPersona(histPerson, datetime);
+            return new MementoPersona(new PersonaMem(Edad, Nombre, Apellido, Profesion), datetime);
         }
     }
 }
