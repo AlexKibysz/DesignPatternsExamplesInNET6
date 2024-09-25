b1343e755f4ad04dfeb50b17b2ad6128f7c70d34
Alex <Alexander.Kibysz@outlook.com>
2024-09-25 10:55:47 -0300

    Add the client code

--- PatronesComportamiento/4.1MediatorChat/Mediator/ChatMediator.cs
+++ PatronesComportamiento/4.1MediatorChat/Mediator/ChatMediator.cs
@@ -35,3 +35,29 @@ namespace PatronesComportamiento._4._1MediatorChat.Mediator
 
     }
 }
+
+/*program.cs
+   IChatMediator chatMediator = new ChatMediator();
+
+        IUser user1 = new User("Alice", chatMediator);
+        IUser user2 = new User("Bob", chatMediator);
+        IUser user3 = new User("Charlie", chatMediator);
+
+        chatMediator.AddUser(user1);
+        chatMediator.AddUser(user2);
+        chatMediator.AddUser(user3);
+
+        user1.SendMessage("Hola a todos!");
+        user2.SendMessage("Hola Alice!");
+        user3.SendMessage("Hola Bob!");
+
+        // Enviar un mensaje directo de Alice a Bob
+        chatMediator.SendDirectMessage("¿Cómo estás, Bob?", user1, user2);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
